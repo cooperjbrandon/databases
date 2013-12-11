1d98095dd6834926a24e966c59d2f0f2bb084c70
Daniel Chao <daniel.h.chao@gmail.com>
2013-12-11 15:11:00 -0800

    schema

--- SQL/schema.sql
+++ SQL/schema.sql
@@ -1,9 +1,30 @@
-CREATE DATABASE chat;
+drop database if exists `chat`;
 
-USE chat;
+create database chat;
 
-CREATE TABLE messages (
- /* Describe your table here.*/
+use chat;
+
+drop table if exists `messages`;
+
+create table messages (
+  `body` varchar(255) not null,
+  `user_id` int(6) not null,
+  `createdAt` date not null
+);
+
+drop table if exists `users`;
+
+create table users (
+  `name` varchar(30) not null,
+  `chatroom_id` int(6) default null,
+  `createdAt` date not null
+);
+
+drop table if exists `chatrooms`;
+
+create table chatrooms (
+  `name` varchar(30) not null,
+  `createdAt` date not null
 );
 
 /* You can also create more tables, if you need them... */
